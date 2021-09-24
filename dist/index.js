db0730bca2b3792d2cd51539f5df77fd6bbfeb3e
Cervellini <diego.cervellini@medgroup.it>
2021-09-24 12:12:13 +0200

    fix

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,168 @@
 function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
 
+var axios = _interopDefault(require('axios'));
 var React = require('react');
 var React__default = _interopDefault(React);
 var ej2ReactCalendars = require('@syncfusion/ej2-react-calendars');
 var ej2ReactSchedule = require('@syncfusion/ej2-react-schedule');
 var moment = _interopDefault(require('moment'));
 var reactBootstrap = require('react-bootstrap');
+var reactTable = require('react-table');
+var reactFontawesome = require('@fortawesome/react-fontawesome');
+var freeSolidSvgIcons = require('@fortawesome/free-solid-svg-icons');
+var styled = _interopDefault(require('styled-components'));
+var _$1 = _interopDefault(require('lodash'));
+require('bootstrap/dist/css/bootstrap.min.css');
 var LoadingOverlay = _interopDefault(require('react-loading-overlay'));
+
+var API_SB_BASE_URL = "https://sbapi.orbital.cloud";
+
+var APISb = /*#__PURE__*/function () {
+  function APISb() {}
+
+  APISb.get = function get(token, endpoint, params, contentType) {
+    var url = API_SB_BASE_URL + endpoint;
+    contentType = contentType || "application/json";
+    var configHeaders = {
+      "content-type": contentType,
+      "Accept": "application/json",
+      "Authorization": "Bearer " + token
+    };
+    var config = {
+      method: "get",
+      url: url,
+      params: params || {},
+      headers: configHeaders
+    };
+    return new Promise(function (resolve, reject) {
+      axios(config).then(function (result) {
+        resolve(result);
+      })["catch"](function (error) {
+        reject(error);
+      });
+    });
+  };
+
+  APISb.post = function post(token, endpoint, params, data, contentType) {
+    var url = API_SB_BASE_URL + endpoint;
+    contentType = contentType || "application/json";
+    var configHeaders = {
+      "content-type": contentType,
+      "Accept": "application/json",
+      "Authorization": "Bearer " + token
+    };
+    var config = {
+      method: "post",
+      url: url,
+      params: params || {},
+      data: data,
+      headers: configHeaders
+    };
+    return new Promise(function (resolve, reject) {
+      axios(config).then(function (result) {
+        resolve(result);
+      })["catch"](function (error) {
+        reject(error);
+      });
+    });
+  };
+
+  APISb.put = function put(token, endpoint, params, data, contentType) {
+    var url = API_SB_BASE_URL + endpoint;
+    contentType = contentType || "application/json";
+    var configHeaders = {
+      "content-type": contentType,
+      "Accept": "application/json",
+      "Authorization": "Bearer " + token
+    };
+    var config = {
+      method: "put",
+      url: url,
+      params: params || {},
+      data: data,
+      headers: configHeaders
+    };
+    return new Promise(function (resolve, reject) {
+      axios(config).then(function (result) {
+        resolve(result);
+      })["catch"](function (error) {
+        reject(error);
+      });
+    });
+  };
+
+  APISb.get_plain = function get_plain(endpoint, params, contentType) {
+    var url = API_SB_BASE_URL + endpoint;
+    contentType = contentType || "application/json";
+    var configHeaders = {
+      "content-type": contentType,
+      "Accept": "application/json"
+    };
+    var config = {
+      method: "get",
+      url: url,
+      params: params || {},
+      headers: configHeaders
+    };
+    return new Promise(function (resolve, reject) {
+      axios(config).then(function (result) {
+        resolve(result);
+      })["catch"](function (error) {
+        reject(error);
+      });
+    });
+  };
+
+  APISb.post_plain = function post_plain(endpoint, params, data, contentType) {
+    var url = API_SB_BASE_URL + endpoint;
+    contentType = contentType || "application/json";
+    var configHeaders = {
+      "content-type": contentType,
+      "Accept": "application/json"
+    };
+    var config = {
+      method: "post",
+      url: url,
+      params: params || {},
+      data: data,
+      headers: configHeaders
+    };
+    return new Promise(function (resolve, reject) {
+      axios(config).then(function (result) {
+        resolve(result);
+      })["catch"](function (error) {
+        reject(error);
+      });
+    });
+  };
+
+  APISb.put_plain = function put_plain(endpoint, params, data, contentType) {
+    var url = API_SB_BASE_URL + endpoint;
+    contentType = contentType || "application/json";
+    var configHeaders = {
+      "content-type": contentType,
+      "Accept": "application/json"
+    };
+    var config = {
+      method: "put",
+      url: url,
+      params: params || {},
+      data: data,
+      headers: configHeaders
+    };
+    return new Promise(function (resolve, reject) {
+      axios(config).then(function (result) {
+        resolve(result);
+      })["catch"](function (error) {
+        reject(error);
+      });
+    });
+  };
+
+  return APISb;
+}();
+
+APISb.API_SB_BASE_URL = API_SB_BASE_URL;
 
 function _extends() {
   _extends = Object.assign || function (target) {
@@ -38,6 +194,15 @@ function _assertThisInitialized(self) {
   }
 
   return self;
+}
+
+function _taggedTemplateLiteralLoose(strings, raw) {
+  if (!raw) {
+    raw = strings.slice(0);
+  }
+
+  strings.raw = raw;
+  return strings;
 }
 
 var DatePicker = /*#__PURE__*/function (_Component) {
@@ -424,6 +589,249 @@ var CustomTooltip = /*#__PURE__*/function (_Component) {
   return CustomTooltip;
 }(React.Component);
 
+function _templateObject() {
+  var data = _taggedTemplateLiteralLoose(["\n  .table {\n    display: inline-block;\n    border-spacing: 0;\n    border: 1px solid #dee2e6;\n\n    .tr {\n      :last-child {\n        .td {\n          border-bottom: 0;\n        }\n      }\n      .td{\n        overflow-x: hidden;\n      }\n    }\n\n    .th,\n    .td {\n      margin: 0;\n      padding: 0.5rem;\n      border-bottom: 1px solid #dee2e6;\n      border-right: 1px solid #dee2e6;\n\n      ", "\n      position: relative;\n\n      :last-child {\n        border-right: 0;\n      }\n\n      .resizer {\n        right: 0;\n        background: #dee2e6;\n        width: 1px;\n        height: 100%;\n        position: absolute;\n        top: 0;\n        z-index: 1;\n        ", "\n        touch-action :none;\n\n        &.isResizing {\n          background: black;\n        }\n      }\n    }\n  }\n"]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+var Styles = styled.div(_templateObject(), '', '');
+
+function ReactTable(_ref) {
+  var localization = _ref.localization,
+      columns = _ref.columns,
+      data = _ref.data,
+      _defaultPageSize = _ref._defaultPageSize,
+      _fixedPageSize = _ref._fixedPageSize,
+      _noDataMessage = _ref._noDataMessage;
+
+  var _useTable = reactTable.useTable({
+    columns: columns,
+    data: data,
+    initialState: {
+      pageIndex: 0,
+      pageSize: _fixedPageSize || _defaultPageSize || 10
+    }
+  }, reactTable.useSortBy, reactTable.usePagination, reactTable.useResizeColumns, reactTable.useFlexLayout, reactTable.useRowSelect),
+      getTableProps = _useTable.getTableProps,
+      getTableBodyProps = _useTable.getTableBodyProps,
+      headerGroups = _useTable.headerGroups,
+      prepareRow = _useTable.prepareRow,
+      page = _useTable.page,
+      canPreviousPage = _useTable.canPreviousPage,
+      canNextPage = _useTable.canNextPage,
+      pageOptions = _useTable.pageOptions,
+      pageCount = _useTable.pageCount,
+      gotoPage = _useTable.gotoPage,
+      nextPage = _useTable.nextPage,
+      previousPage = _useTable.previousPage,
+      setPageSize = _useTable.setPageSize,
+      _useTable$state = _useTable.state,
+      pageIndex = _useTable$state.pageIndex,
+      pageSize = _useTable$state.pageSize;
+
+  var setPageSizeOptions = function setPageSizeOptions() {
+    var base_values = [5, 10, 20, 30, 40, 50];
+
+    if (_$1.includes(base_values, _defaultPageSize) == false) {
+      base_values.push(_defaultPageSize);
+      base_values = _$1.sortBy(base_values);
+    }
+
+    if (_$1.includes(base_values, _fixedPageSize) == false) {
+      base_values.push(_fixedPageSize);
+      base_values = _$1.sortBy(base_values);
+    }
+
+    var options = _$1.map(base_values, function (value, index) {
+      return /*#__PURE__*/React__default.createElement("option", {
+        key: index,
+        value: value
+      }, value);
+    });
+
+    return options;
+  };
+
+  var setSortIcon = function setSortIcon(column) {
+    return /*#__PURE__*/React__default.createElement(Fragment, null, column.disableSortBy != true && column.isSorted == false && /*#__PURE__*/React__default.createElement("span", null, ' ', /*#__PURE__*/React__default.createElement(reactFontawesome.FontAwesomeIcon, {
+      icon: freeSolidSvgIcons.faSort,
+      style: {
+        cursor: "pointer"
+      },
+      onClick: function onClick() {
+        column.toggleSortBy();
+      }
+    })), /*#__PURE__*/React__default.createElement("span", null, column.isSorted ? column.isSortedDesc ? /*#__PURE__*/React__default.createElement("span", null, ' ', /*#__PURE__*/React__default.createElement(reactFontawesome.FontAwesomeIcon, {
+      icon: freeSolidSvgIcons.faSortDown,
+      style: {
+        cursor: "pointer"
+      },
+      onClick: function onClick() {
+        column.toggleSortBy();
+      }
+    })) : /*#__PURE__*/React__default.createElement("span", null, ' ', /*#__PURE__*/React__default.createElement(reactFontawesome.FontAwesomeIcon, {
+      icon: freeSolidSvgIcons.faSortUp,
+      style: {
+        cursor: "pointer"
+      },
+      onClick: function onClick() {
+        column.toggleSortBy();
+      }
+    })) : ''));
+  };
+
+  var setResize = function setResize(column) {
+    return /*#__PURE__*/React__default.createElement(Fragment, null, column.canResize && /*#__PURE__*/React__default.createElement("div", _extends({}, column.getResizerProps(), {
+      className: "resizer " + (column.isResizing ? "isResizing" : "")
+    })));
+  };
+
+  var setEmptyRows = function setEmptyRows() {
+    var rows = null;
+
+    if (canNextPage == false && page.length < pageSize && page[0]) {
+      var new_filling_rows = pageSize - page.length;
+      new_filling_rows = new Array(new_filling_rows);
+
+      var page_tmp = _$1.map(new_filling_rows, function (f) {
+        return page[0];
+      });
+
+      rows = page_tmp.map(function (row, i) {
+        var new_id = data.length + i;
+        row.id = new_id;
+        prepareRow(row);
+        return /*#__PURE__*/React__default.createElement("div", _extends({}, row.getRowProps(), {
+          className: "tr"
+        }), row.cells.map(function (cell) {
+          return /*#__PURE__*/React__default.createElement("div", _extends({}, cell.getCellProps(), {
+            className: "td"
+          }), /*#__PURE__*/React__default.createElement("div", {
+            style: {
+              color: "#66000000"
+            }
+          }, "."));
+        }));
+      });
+    }
+
+    return rows;
+  };
+
+  var setEmptyHeaders = function setEmptyHeaders() {
+    var new_filling_rows = new Array(pageSize);
+
+    var rows = _$1.map(new_filling_rows, function () {
+      return headerGroups.map(function (headerGroup) {
+        return /*#__PURE__*/React__default.createElement("div", _extends({}, headerGroup.getHeaderGroupProps(), {
+          className: "tr"
+        }), headerGroup.headers.map(function (column) {
+          return /*#__PURE__*/React__default.createElement("div", _extends({}, column.getHeaderProps(), {
+            className: "th"
+          }), /*#__PURE__*/React__default.createElement("div", {
+            style: {
+              color: "#66000000"
+            }
+          }, "."));
+        }));
+      });
+    });
+
+    return rows;
+  };
+
+  return /*#__PURE__*/React__default.createElement(Styles, null, /*#__PURE__*/React__default.createElement("div", null, /*#__PURE__*/React__default.createElement("div", _extends({}, getTableProps(), {
+    className: "table"
+  }), /*#__PURE__*/React__default.createElement("div", null, headerGroups.map(function (headerGroup) {
+    return /*#__PURE__*/React__default.createElement("div", _extends({}, headerGroup.getHeaderGroupProps(), {
+      className: "tr"
+    }), headerGroup.headers.map(function (column) {
+      return /*#__PURE__*/React__default.createElement("div", _extends({}, column.getHeaderProps(), {
+        className: "th"
+      }), column.render('Header'), setSortIcon(column), setResize(column));
+    }));
+  }), data.length == 0 && /*#__PURE__*/React__default.createElement(Fragment, null, setEmptyHeaders())), /*#__PURE__*/React__default.createElement("div", getTableBodyProps(), page.map(function (row, i) {
+    prepareRow(row);
+    return /*#__PURE__*/React__default.createElement("div", _extends({}, row.getRowProps(), {
+      className: "tr"
+    }), row.cells.map(function (cell) {
+      return /*#__PURE__*/React__default.createElement("div", _extends({}, cell.getCellProps(), {
+        className: "td"
+      }), cell.render('Cell'));
+    }));
+  }), data.length > 0 && /*#__PURE__*/React__default.createElement(Fragment, null, setEmptyRows())), data.length == 0 && /*#__PURE__*/React__default.createElement("div", {
+    className: "noData"
+  }, /*#__PURE__*/React__default.createElement(reactFontawesome.FontAwesomeIcon, {
+    icon: freeSolidSvgIcons.faInfoCircle
+  }), " ", _noDataMessage || "No data"))), /*#__PURE__*/React__default.createElement(reactBootstrap.Row, {
+    className: "pagination"
+  }, /*#__PURE__*/React__default.createElement(reactBootstrap.Col, {
+    sm: 8
+  }, /*#__PURE__*/React__default.createElement(Fragment, null, /*#__PURE__*/React__default.createElement(reactBootstrap.Button, {
+    variant: "outline-secondary",
+    size: "sm",
+    onClick: function onClick() {
+      return gotoPage(0);
+    },
+    disabled: !canPreviousPage
+  }, " ", '<<'), ' ', /*#__PURE__*/React__default.createElement(reactBootstrap.Button, {
+    variant: "outline-secondary",
+    size: "sm",
+    onClick: function onClick() {
+      return previousPage();
+    },
+    disabled: !canPreviousPage
+  }, " ", '<'), ' ', /*#__PURE__*/React__default.createElement(reactBootstrap.Button, {
+    variant: "outline-secondary",
+    size: "sm",
+    onClick: function onClick() {
+      return nextPage();
+    },
+    disabled: !canNextPage
+  }, " ", '>'), ' ', /*#__PURE__*/React__default.createElement(reactBootstrap.Button, {
+    variant: "outline-secondary",
+    size: "sm",
+    onClick: function onClick() {
+      return gotoPage(pageCount - 1);
+    },
+    disabled: !canNextPage
+  }, " ", '>>'), ' ', /*#__PURE__*/React__default.createElement("span", {
+    style: {
+      "float": "right"
+    }
+  }, /*#__PURE__*/React__default.createElement("span", null, localization.page || "Page", ' ', /*#__PURE__*/React__default.createElement("strong", null, pageIndex + 1, "  ", localization.of || "of", " ", pageOptions.length), ' '), /*#__PURE__*/React__default.createElement("span", null, "| ", localization.go_to_page, ":", ' ')))), /*#__PURE__*/React__default.createElement(reactBootstrap.Col, {
+    sm: 2
+  }, /*#__PURE__*/React__default.createElement(reactBootstrap.Form.Control, {
+    disabled: data.length == 0,
+    size: "sm",
+    type: "number",
+    min: 1,
+    max: pageOptions.length,
+    defaultValue: pageIndex + 1,
+    onChange: function onChange(e) {
+      var page = e.target.value ? Number(e.target.value) - 1 : 0;
+      gotoPage(page);
+    },
+    style: {
+      width: '100%'
+    }
+  })), /*#__PURE__*/React__default.createElement(reactBootstrap.Col, {
+    sm: 2
+  }, /*#__PURE__*/React__default.createElement(reactBootstrap.Form.Control, {
+    as: "select",
+    size: "sm",
+    value: pageSize,
+    disabled: _fixedPageSize != null || data.length == 0,
+    onChange: function onChange(e) {
+      setPageSize(Number(e.target.value));
+    }
+  }, setPageSizeOptions()))));
+}
+
 var CustomLoadingOverlay = /*#__PURE__*/function (_Component) {
   _inheritsLoose(CustomLoadingOverlay, _Component);
 
@@ -441,9 +849,11 @@ var CustomLoadingOverlay = /*#__PURE__*/function (_Component) {
   return CustomLoadingOverlay;
 }(React.Component);
 
+exports.APISb = APISb;
 exports.DatePicker = DatePicker;
 exports.DateTimePicker = DatePicker$1;
 exports.LoadingOverlay = CustomLoadingOverlay;
+exports.ReactTable = ReactTable;
 exports.RecurrenceEditor = RecurrenceEditor;
 exports.Scheduler = ReservationScheduler;
 exports.TimePicker = TimePicker;
