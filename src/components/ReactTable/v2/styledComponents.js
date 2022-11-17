6655b934503be850cf192274e88d5fce57b33caf
Diego Cervellini <diego.cervellini@medgroup.it>
2022-11-17 09:16:04 +0100

    fix

--- src/components/ReactTable/v2/styledComponents.js
+++ src/components/ReactTable/v2/styledComponents.js
@@ -2,99 +2,95 @@ import React from "react";
 import styled from "styled-components";
 import { Row } from "react-bootstrap";
 
-function NoData(props) {
-    const StyledDiv = styled.div`
-        display: block;
-        position: absolute;
-        left: 50%;
-        top: 40%;
-        z-index: 1;
-        padding: 20px;
-        background-color: white;
-        border: 1px solid #dee2e6;
-    `;
+const StyledDivNoData = styled.div`
+display: block;
+position: absolute;
+left: 50%;
+top: 40%;
+z-index: 1;
+padding: 20px;
+background-color: white;
+border: 1px solid #dee2e6;
+`;
 
-    return <StyledDiv>{props.children}</StyledDiv>
+function NoData(props) {
+    return <StyledDivNoData>{props.children}</StyledDivNoData>
 }
+
+
+const StyledDivSubContentContainer = styled.div`
+    border-bottom: 1px solid #dee2e6;
+    padding: 15px;
+`;
 
 function SubContentContainer(props) {
-    const StyledDiv = styled.div`
-        border-bottom: 1px solid #dee2e6;
-        padding: 15px;
-    `;
-
-    return <SubContentContainer>{props.children}</SubContentContainer>
+    return <StyledDivSubContentContainer>{props.children}</StyledDivSubContentContainer>
 }
+
+const StyledDivTable = styled.div`
+    display: inline-block;
+    border-spacing: 0;
+    border: 1px solid #dee2e6;
+    width: 100%;
+`;
 
 function StyledTable(props) {
-    const StyledDiv = styled.div`
-        display: inline-block;
-        border-spacing: 0;
-        border: 1px solid #dee2e6;
-        width: 100%;
-    `;
-
-    return <StyledDiv {...props}>{props.children}</StyledDiv>
+    return <StyledDivTable {...props}>{props.children}</StyledDivTable>
 }
 
+const StyledDivTd = styled.div`
+    margin: 0;
+    padding: 0.5rem;
+    border-bottom: 1px solid #dee2e6;
+    border-right: 1px solid #dee2e6;
+    position: relative;
+`;
 function StyledTd(props) {
-    const StyledDiv = styled.div`
-        margin: 0;
-        padding: 0.5rem;
-        border-bottom: 1px solid #dee2e6;
-        border-right: 1px solid #dee2e6;
-        position: relative;
-    `;
-
-    return <StyledDiv {...props}>{props.children}</StyledDiv>
+    return <StyledDivTd {...props}>{props.children}</StyledDivTd>
 }
 
+const StyledDivTh = styled.div`
+    margin: 0;
+    padding: 0.5rem;
+    border-bottom: 1px solid #dee2e6;
+    border-right: 1px solid #dee2e6;
+    position: relative;
+`;
 function StyledTh(props) {
-    const StyledDiv = styled.div`
-        margin: 0;
-        padding: 0.5rem;
-        border-bottom: 1px solid #dee2e6;
-        border-right: 1px solid #dee2e6;
-        position: relative;
-    `;
-
-    return <StyledDiv {...props}>{props.children}</StyledDiv>
+    return <StyledDivTh {...props}>{props.children}</StyledDivTh>
 }
 
+const StyledRow = styled(Row)`
+    margin-top: 1rem;
+`;
 function PaginationRow(props) {
-    const StyledRow = styled(Row)`
-        margin-top: 1rem;
-    `;
-
     return <StyledRow {...props}>{props.children}</StyledRow>
 }
 
+const StyledDivResizer = styled.div`
+    right: 0;
+    background: #dee2e6;
+    width: 1px;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    z-index: 1;
+    touch-action: none;
+`;
 function Resizer(props) {
-    const StyledDiv = styled.div`
-        right: 0;
-        background: #dee2e6;
-        width: 1px;
-        height: 100%;
-        position: absolute;
-        top: 0;
-        z-index: 1;
-        touch-action: none;
-    `;
-
-    return <StyledDiv {...props}>{props.children}</StyledDiv>
+    return <StyledDivResizer {...props}>{props.children}</StyledDivResizer>
 }
 
+const StyledDivTr = styled.div`
+&:last-child{
+    overflow-x: hidden;
+    ${StyledTd}{
+        border-bottom: 0;
+    }
+}
+`;
 function StyledTr(props) {
-    const StyledDiv = styled.div`
-        &:last-child{
-            overflow-x: hidden;
-            ${StyledTd}{
-                border-bottom: 0;
-            }
-        }
-    `;
-
-    return <StyledDiv {...props}>{props.children}</StyledDiv>
+    return <StyledDivTr {...props}>{props.children}</StyledDivTr>
 }
 
 export { NoData, SubContentContainer, StyledTable, StyledTd, StyledTh, PaginationRow, Resizer, StyledTr }
